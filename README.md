80e62b2734cd892beba5440ef76749533da3cdea
Emmanuel Akanji <akanjiemmanuel37@gmail.com>
2025-07-13 13:13:54 +0100

    boom

---
+++ README.md
@@ -0,0 +1,8 @@
+# @emmanuel_a37/sentinel
+
+Track and monitor Express.js API usage in real-time with this plug-and-play middleware. Automatically logs method, route, status code, and response time to your Sentinel backend.
+
+## 📦 Installation
+
+```bash
+npm install @emmanuel_a37/sentinel
---
+++ index.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import { performance } from 'perf_hooks';
+
+const logQueue = [];
+
+
+const flushQueue = async () => {
+  const queueCopy = [...logQueue];
+  logQueue.length = 0;
+
+  for (const { url, apiKey, data } of queueCopy) {
+    try {
+      await axios.post(url, data, {
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': apiKey,
+        },
+        timeout: 3000, 
+      });
+    } catch (e) {
+      logQueue.push({ url, apiKey, data }); 
+    }
+  }
+};
+
+setInterval(flushQueue, 5000); // retry every 5s
+
+const sentinelLogger = ({ apiKey, url }) => {
+  if (!apiKey ) {
+    throw new Error('Sentinel Logger: apiKey are required');
+  }
+
+  return (req, res, next) => {
+    const start = performance.now();
+
+    const originalSend = res.send.bind(res);
+    const originalJson = res.json.bind(res);
+
+    const prepareLog = () => {
+      const duration = performance.now() - start;
+      const log = {
+        path: req.baseUrl.toLowerCase() + req.path.toLowerCase(),
+        method: req.method.toUpperCase(),
+        statusCode: res.statusCode,
+        responseTime: Number(duration.toFixed(2)),
+      };
+      logQueue.push({ url, apiKey, data: log });
+    };
+
+    res.send = function (body) {
+      prepareLog();
+      return originalSend(body);
+    };
+
+    res.json = function (body) {
+      prepareLog();
+      return originalJson(body);
+    };
+
+    next();
+  };
+};
+
+export default sentinelLogger;
